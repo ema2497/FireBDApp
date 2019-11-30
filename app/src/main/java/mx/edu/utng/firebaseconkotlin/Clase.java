7ecc13d610adea4b7ac9c28c5c7f5147b89175da
ema2497 <49290161+ema2497@users.noreply.github.com>
2019-11-30 15:45:31 -0600

    Primer commit

---
+++ app/src/main/java/mx/edu/utng/firebaseconkotlin/Clase.java
@@ -0,0 +1,44 @@
+package mx.edu.utng.firebaseconkotlin;
+
+public class Clase {
+    private String claseId, carrera, materia, tema;
+
+    public Clase(String claseId, String carrera, String materia, String tema) {
+        this.claseId = claseId;
+        this.carrera = carrera;
+        this.materia = materia;
+        this.tema = tema;
+    }
+
+    public String getClaseId() {
+        return claseId;
+    }
+
+    public void setClaseId(String claseId) {
+        this.claseId = claseId;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+}
